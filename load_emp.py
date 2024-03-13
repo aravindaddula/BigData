5be9614f8c79b6d084808a19c4a12fb17378b3b2
aravindaddula <aravindaddula@gmail.com>
2024-03-13 10:13:54 +0530

    got descent understanding of spark

--- load_emp.py
+++ load_emp.py
@@ -34,7 +34,10 @@ empdata = df.select("fullname.firstname", "fullname.middlename", "fullname.lastn
 empdata.show(truncate=False)
 
 # Write the DataFrame to ORC format
-empdata.write.format("orc").save("emp_details")
+#empdata.write.format("orc").save("emp/emp_details")
+
+#write data into local Fs
+empdata.write.format("orc").save("file:///home/tony/BigData/Files/emp_details")
 
 # Stop Spark session
 spark.stop()
